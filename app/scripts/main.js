021647be2c1a763ebf45751d7775f8bd496c62ce
Andre Gärtner <andregartner@Andres-MBP.home>
2016-07-03 18:05:12 +0200

    import project

---
+++ app/scripts/main.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+  // ADD SLIDEDOWN ANIMATION TO DROPDOWN //
+  $('.dropdown').on('show.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  });
+
+  // ADD SLIDEUP ANIMATION TO DROPDOWN //
+  $('.dropdown').on('hide.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  });
+
+
+
+  // tooltip animation hover top
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  });
+
+});
